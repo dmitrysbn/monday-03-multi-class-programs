b69be10883e27eee5014bb56b9493d9ba02a8a80
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-06 12:38:11 -0500

    add cart stretch exercises

--- capitalism/cart.rb
+++ capitalism/cart.rb
@@ -7,7 +7,19 @@ class Cart
   end
 
   def add_product(product)
+    # product.number += 1
     @products << product
+  end
+
+  def how_many(product)
+    #go through list @products of products and count how many matches
+    running_total = 0
+    @products.each do |item|
+      if item.name == product.name
+        running_total += 1
+      end
+    end
+    return running_total
   end
 
   def remove_product(product)
@@ -37,17 +49,35 @@ class Cart
     return running_total
   end
 
+  def most_expensive_product
+    product_numbers = @products.map do |product|
+      product.total_price
+    end
+    most_expensive_price = product_numbers.max
+    most_expensive_index = product_numbers.find_index(most_expensive_price)
+    return @products[most_expensive_index]
+  end
+
 
 end
 
-apple = Product.new('apple', 5, 1.13)
-orange = Product.new('orange', 2, 1.13)
+apple = Product.new('apple', 5, 'tax exempt')
+orange = Product.new('orange', 2, 'standard')
+blueberry = Product.new('blueberry', 20, 'standard')
+lol = Product.new('lol', 14, 'imported')
+
 # puts apples.total_price
 
 cart = Cart.new
 cart.add_product(apple)
+cart.add_product(apple)
 cart.add_product(orange)
+cart.add_product(blueberry)
+cart.add_product(lol)
 puts cart.products
+puts cart.most_expensive_product.inspect
 
-puts cart.cost_before_tax
-puts cart.cost_after_tax
+puts cart.how_many(apple)
+
+# puts cart.cost_before_tax
+# puts cart.cost_after_tax
--- capitalism/product.rb
+++ capitalism/product.rb
@@ -1,9 +1,26 @@
 class Product
 
+  @@standard = 1.13
+  @@tax_exempt = 1.00
+  @@imported = 1.20
+  @@number = 1
+
+  def self.number
+    @@number
+  end
+
   def initialize(name, base_price, tax_rate)
     @name = name
     @base_price = base_price
-    @tax_rate = tax_rate
+    if tax_rate == 'standard'
+      @tax_rate = @@standard
+
+    elsif tax_rate == 'tax exempt'
+      @tax_rate = @@tax_exempt
+
+    else
+      @tax_rate = @@imported
+    end
   end
 
   def name
